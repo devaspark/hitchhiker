b048629d95e3035c6aa1d568af406bbd6085cf58
rkung <rex.h.kung@gmail.com>
2018-03-28 23:49:24 -0600

    Add in functionality: error handling

--- hitchhiker/Controller/HomeVC.swift
+++ hitchhiker/Controller/HomeVC.swift
@@ -129,8 +129,17 @@ class HomeVC: UIViewController {
     }
     
     @IBAction func centerMapBtnPressed(_ sender: Any) {
-        centerMapOnUserLocation()
-        centerMapBtn.fadeTo(alphaValue: 0.0, withDuration: 0.2)
+        DataService.instance.REF_USERS.document(currentUID!).getDocument { (docSnapshot, error) in
+            let data = docSnapshot?.data()
+            if data!["tripCoordinate"] != nil {
+                self.zoom(toFitAnnotationsFromMapView: self.mapView)
+                self.centerMapBtn.fadeTo(alphaValue: 0.0, withDuration: 0.2)
+            } else {
+                self.centerMapOnUserLocation()
+                self.centerMapBtn.fadeTo(alphaValue: 0.0, withDuration: 0.2)
+            }
+        }
+
     }
 }
 
@@ -197,6 +206,7 @@ extension HomeVC: MKMapViewDelegate {
         let lineRenderer = MKPolylineRenderer(polyline: self.route.polyline)
         lineRenderer.strokeColor = UIColor(red: 216/255, green: 71/255, blue: 30/255, alpha: 0.75)
         lineRenderer.lineWidth = 3
+        zoom(toFitAnnotationsFromMapView: self.mapView)
         return lineRenderer
     }
     
@@ -256,7 +266,30 @@ extension HomeVC: MKMapViewDelegate {
             self.shouldPresentLoadingView(false)
         }
     }
+    
+    func zoom(toFitAnnotationsFromMapView mapView: MKMapView) {
+        if mapView.annotations.count == 0 {
+            return
+        }
+        
+        var topLeftCoordinate = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+        var bottomRightCoordinate = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+        
+        for annotation in mapView.annotations where !annotation.isKind(of: DriverAnnotation.self) {
+            topLeftCoordinate.longitude = fmin(topLeftCoordinate.longitude, annotation.coordinate.longitude)
+            topLeftCoordinate.latitude = fmax(topLeftCoordinate.latitude, annotation.coordinate.latitude)
+            bottomRightCoordinate.longitude = fmax(bottomRightCoordinate.longitude, annotation.coordinate.longitude)
+            bottomRightCoordinate.latitude = fmin(bottomRightCoordinate.latitude, annotation.coordinate.latitude)
+        }
+        
+        var region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(topLeftCoordinate.latitude - ((topLeftCoordinate.latitude - bottomRightCoordinate.latitude) * 0.5), topLeftCoordinate.longitude + ((bottomRightCoordinate.longitude - topLeftCoordinate.longitude) * 0.5)), span: MKCoordinateSpan(latitudeDelta: fabs(topLeftCoordinate.latitude-bottomRightCoordinate.latitude) * 2.0, longitudeDelta: fabs(bottomRightCoordinate.longitude - topLeftCoordinate.longitude) * 2.0))
+        
+        region = mapView.regionThatFits(region)
+        mapView.setRegion(region, animated: true)
+    }
 }
+
+
 
 extension HomeVC: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
---
+++ hitchhiker/Protocols/Alertable.swift
@@ -0,0 +1,20 @@
+//
+//  Alertable.swift
+//  hitchhiker
+//
+//  Created by Rex Kung on 3/28/18.
+//  Copyright © 2018 Rex Kung. All rights reserved.
+//
+
+import UIKit
+
+protocol Alertable {}
+
+extension Alertable where Self: UIViewController {
+    func showAlert(_ msg: String) {
+        let alertController = UIAlertController(title: "Error:", message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(action)
+        present(alertController, animated: true, completion: nil)
+    }
+}
